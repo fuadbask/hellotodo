dddb01838bbf79fdef55160ef1534de28f3b64d6
Fuad Baskara P <fuadbaskara@gmail.com>
2018-04-10 18:13:51 +0700

    put edit function to alive

--- scripts/app.js
+++ scripts/app.js
@@ -1,11 +1,13 @@
 const outputBox = document.getElementById("outputBox");
 const addButton = document.getElementById("submitAll");
+const searchText = document.getElementById("searchText");
+const todoListener = document.getElementById("listener");
 const parser = new DOMParser();
 
 function submitNewTodo() {
   const activity = document.getElementById("activityText").value;
 
-  if (activity !== "" ) {
+  if (activity !== "") {
     // push new contact to storage
     addTodo({
       activity: activity
@@ -38,17 +40,31 @@ function setTodo(newTodos) {
 }
 
 function createTemplate(index, arrayList) {
+  // console.log(arrayList.activity);
   const template = `
   <div class="margin">
-  <div id="todo-${index}">
-    <p for="activities">${arrayList.activity}
-      <button name="delete" class="red">X</button>
-    </p>
+    <div id="todo-${index}" class="todo">
+      <div id="listener" class="listener">
+        <p id="par-${index}" class="par" for="activities">${arrayList.activity}</p>
+        <input id="edit-${index}" type="text" class="editText" value="${arrayList.activity}">
+        <button id="delete-${index}" name="delete" class="delete red">X</button>
+      </div>
+    </div>
   </div>
     `;
   return template;
 }
 // <input id="edit" class="Edit" onclick=editTodo(${index})>
+// <div class="margin">
+// <div id="todo-${index}" class="todo">
+//   <div id="listener" class="listener">
+//   <p id="par-${index}" class="par" for="activities">${arrayList.activity}
+//     <input id="edit-${index}" type="text" class="editText" value="${arrayList.activity}">
+//     <button id="delete-${index}" name="delete" class="delete red">X</button>
+//   </p>
+//   </div>
+// </div>
+// </div>
 
 function displayTodo() {
   outputBox.innerHTML = "";
@@ -64,13 +80,63 @@ function displayTodo() {
 function submitTodo() {
   submitNewTodo();
   displayTodo();
+  toggleHandler();
+}
+
+function deleteTodo() {
+  if (event.target.matches(".delete")) {
+    // console.log("CALLED DELETE");
+    const id = event.target.id.replace("delete-", "");
+    const todos = getTodo();
+    todos.splice(id, 1);
+    setTodo(todos);
+    displayTodo();
+    toggleHandler();
+  }
+}
+
+function editTodo() {
+  const toggleEditText = document.getElementsByClassName("editText");
+  const toggleTodo = document.getElementsByClassName("par");
+  if (event.target.matches(".par")) {
+    const id = event.target.id.replace("par-", "");
+    // console.log(id);
+    toggleTodo[id].style.display = 'none';
+    toggleEditText[id].style.display = 'block';
+    // console.log(todo);
+    saveEditTodo(id);
+  }
+}
+
+function saveEditTodo(id) {
+  // console.log(id);
+  var input = document.getElementsByClassName("editText")[id];
+  // console.log(input);
+  input.addEventListener("keyup", function(event) {
+    event.preventDefault();
+    if (event.keyCode === 13) {
+      const todo = document.getElementsByClassName("editText")[id].value;
+      console.log(todo);
+      const arrayList = getTodo();
+      arrayList.splice(id, 1, {activity:todo});
+      console.log(arrayList);
+      setTodo(arrayList);
+      displayTodo();
+      toggleHandler();
+    }
+  });
 }
 
 function toggleHandler() {
-  const toggleSearchText = document.getElementById("searchText");
-  toggleSave.style.display = "none";
+  const toggleEdit = document.getElementsByClassName("editText");
+  for (var i = 0; i < toggleEdit.length; i++) {
+    toggleEdit[i].style.display = 'none';
+  };
 }
 
-getTodo();
 displayTodo();
+toggleHandler();
+
 addButton.addEventListener("click", submitTodo);
+outputBox.addEventListener("click", deleteTodo);
+outputBox.addEventListener("dblclick", editTodo);
